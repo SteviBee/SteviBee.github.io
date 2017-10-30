5105f8112b8be66eaa510cced8d10d3089964a38
Stephen Borutta <33042374+SteviBee@users.noreply.github.com>
2017-10-30 09:45:54 -0500

    Adding a new file showing everything i learned today.

---
+++ test.py
@@ -0,0 +1,13 @@
+#test file to see how to upload from my text editor to github
+print "Things I learned today: "
+print "hello world".title()
+x = 5
+y = (1, 2, 3, 4, 5)
+b = (2, 4, 6, 8, 10)
+
+if x in (y, b):
+    print "%d is in one of these: %r, %r" % (x, y, b)
+else:
+    print "%d is not in anything!" % x
+
+print "One more thing!"
